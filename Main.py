d3c254145437292c01e8a92c83ecd82926548352
Bence Szucs <bence.szucs200625@gmail.com>
2024-02-15 11:20:37 -0500

    adds pygame example code

---
+++ Main.py
@@ -0,0 +1,28 @@
+import pygame
+pygame.init()
+
+#Set up size of screen
+screenWidth = 800
+screenHeight = 800
+screen = pygame.display.set_mode([screenWidth,screenHeight])
+
+running = True
+
+while running:
+    
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
