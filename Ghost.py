1b32014d4d504e4e2d58debc9789648223ca1c17
Bence Szucs <bence.szucs200625@gmail.com>
2024-02-18 18:50:47 -0500

    adds enemy movement

--- Ghost.py
+++ Ghost.py
@@ -1,28 +1,41 @@
 import pygame
 import random
+import Constants as Const
 
 class Ghost(pygame.sprite.Sprite):
 
     isScared : bool
+    direction : int
 
     def __init__(self, colour):
         super(Ghost, self).__init__()
-        self.surf = pygame.Surface((20,10))
+        self.surf = pygame.Surface((25,25))
         self.surf.fill((255,255,255))
         self.rect = self.surf.get_rect(
             center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
+                random.randint(Const.screenWidth + 20, Const.screenWidth + 100),
+                random.randint(0, Const.screenHeight),
             )
         )
-        self.speed = random.randint(5, 20)
+        self.speed = 5
         self.colour = colour
     # Move the sprite based on speed
     # Remove the sprite when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-self.speed, 0)
+    def update(self, playerRight):
+        
+        if self.rect.right < playerRight:
+            self.direction = 1
+        elif self.rect.right > playerRight:
+            self.direction = 2
+        else:
+            self.direction = 0
+        
+        if self.direction == 1:
+            self.rect.move_ip(self.speed, 0)
+        elif self.direction == 2:
+            self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
     
-    def scared():
-        isScared = True
+    def scared(self):
+        self.isScared = True
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
 import Constants as Const
 import Player
+import Ghost
 pygame.init()
 
 from pygame.locals import (
@@ -19,9 +20,17 @@ clock = pygame.time.Clock()
 
 player = Player.Player()
 
+
+# Enemy update group
+enemySprites = pygame.sprite.Group()
+
 # Sprite rendering group
 allSprites = pygame.sprite.Group()
 allSprites.add(player)
+for x in range(5):
+    ghost = Ghost.Ghost((255,255,255))
+    enemySprites.add(ghost)
+    allSprites.add(ghost)
 
 running = True
 
@@ -35,11 +44,16 @@ while running:
     pressed_keys = pygame.key.get_pressed()
     #updates the players position based on user input
     player.update(pressed_keys)
+    for enemy in enemySprites:
+        enemy.update(player.rect.right)
     #fills the background as black
     screen.fill((0,0,0))
     #puts the player on the screen
     for entity in allSprites:
         screen.blit(entity.surf, entity.rect)
+
+    if pygame.sprite.spritecollideany(player, enemySprites):
+        player.kill()
     # sets the framerate
     clock.tick(30)
     #updates the screen
