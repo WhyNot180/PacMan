1f30b0c8370aa131846ed92942b63df7582fce2c
CH0C0M1LK <127130183+CH0C0M1LK@users.noreply.github.com>
2024-02-21 11:31:37 -0500

    Update collectables.py

--- collectables.py
+++ collectables.py
@@ -10,34 +10,45 @@ DISPLAYSURF.fill((255, 255, 255))
 class collectables(pygame.sprite.Sprite):
   def __init__(self, position_x, position_y, point_value):
     super().__init__()
-    self.image = pygame.Surface([50, 50])
-    self.image.fill((111, 0, 0))
+    self.image = pygame.Surface([25, 25])
+    self.image.fill((0, 0, 0))
     self.rect = self.image.get_rect()
     self.position_x = position_x
     self.position_y = position_y
     self.point_value = point_value
+    self.rect.x = position_x
+    self.rect.y = position_y
 
-test = collectables(100, 100, 10)
-test2 = collectables(200, 200, 10)
+test = collectables(50, 100, 10)
+test2 = collectables(100, 100, 10)
 
-#class pellet(collectables):
-#      super(). __init__ (position_x, position_y, point_value)
-#      self.point_value = 10
-#      self.level_up = False
-#      self.spawn_fruit = False
+class pellet(collectables):
+   def __init__ (self, position_x, position_y, point_value):
+     super().__init__(position_x, position_y, point_value)
+     self.image.fill((200, 0, 0))
+     self.point_value = 10
+     self.level_up = False 
+     self.spawn_fruit = False
 
-#class power_pellet(collectables):
-#    def __init__ (self, position_x, position_y, point_value):
-#      super(). __init__ 
-#      self.point_value = 50
-#      self.power_up = False
+test3 = pellet(150, 100, 10)
 
-#class fruit(collectables):
-#    def __init__ (self, position_x, position_y, point_value):
-#      super(). __init__ 
-#      self.point_value = 100
-#      self.appear = False
-#      pass
+class power_pellet(collectables):
+    def __init__ (self, position_x, position_y, point_value):
+      super(). __init__ (position_x, position_y, point_value)
+      self.image.fill((0, 200, 0))
+      self.point_value = 50
+      self.power_up = False
+
+test4 = power_pellet(200, 100, 10)
+
+class fruit(collectables):
+    def __init__ (self, position_x, position_y, point_value):
+      super(). __init__ (position_x, position_y, point_value)
+      self.image.fill((0, 0, 200))
+      self.point_value = 100
+      self.appear = False
+
+test5 = fruit(250, 100, 10)
 
 pygame.display.set_caption('Hello World!')
 while True:
@@ -45,7 +56,10 @@ while True:
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
-      test.draw()
   DISPLAYSURF.blit(test.image, test.rect)
+  DISPLAYSURF.blit(test2.image, test2.rect)
+  DISPLAYSURF.blit(test3.image, test3.rect)
+  DISPLAYSURF.blit(test4.image, test4.rect)
+  DISPLAYSURF.blit(test5.image, test5.rect)
   pygame.display.flip()
   #pygame.display.update()
