579f7d8b5223593c7ea525413810000a507125ec
SimonPorcellato <127326443+SimonPorcellato@users.noreply.github.com>
2024-02-22 16:01:59 +0000

    adds grid

---
+++ Map.py
@@ -0,0 +1,47 @@
+# map class
+import pygame
+
+#Set up size of screen
+screenWidth = 448
+screenHeight = 576
+screen = pygame.display.set_mode([screenWidth,screenHeight])
+#Set up background
+class Fixed_object:
+    def __init__(self, columns, rows):
+        self.column = columns
+        self.row = rows
+
+
+
+class Grid:
+    def __init__(self, columns, rows, width):
+        self.columns = 40
+        self.rows = 60
+        self.width = screenWidth / self.columns
+      
+    def draw(self):
+      for i in range(self.columns):
+        for j in range(self.rows):
+         pygame.Rect(i * self.width, j * self.width, self.width, self.width)
+
+
+#class Border(Fixed_object):
+   # def __init__(self, colour):
+      #  super().__init__(self.column, self.row)
+       # self.colour = colour
+
+    #def draw(self):
+        
+
+
+#class Obstacol(Fixed_object):
+    #def __init__(self, colour):
+       # super().__init__(column, row)
+        #self.colour = colour
+
+
+
+#class tunnel:
+
+#class cage:
+        
